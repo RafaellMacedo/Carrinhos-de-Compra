c6772fdc443298951739a16a86c27ec9ffdb8136
Rafael Luiz de Macedo <rafael.macedo-montreal@pbh.gov.br>
2026-02-24 10:36:58 -0300

    Corrigido erro de leitura do arquivo yaml do sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,11 @@
+require 'sidekiq-scheduler'
+
+redis_url = ENV.fetch("REDIS_URL", "redis://redis:6379/0")
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL") { "redis://redis:6379/0" } }
+  config.redis = { url: redis_url }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL") { "redis://redis:6379/0" } }
+  config.redis = { url: redis_url }
 end
--- spec/jobs/abandoned_carts_job_spec.rb
+++ spec/jobs/abandoned_carts_job_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
+require 'sidekiq/testing'
 
 RSpec.describe AbandonedCartsJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    Sidekiq::Testing.inline!
+  end
+
+  let!(:active_cart) { Cart.create!(total_price: 100.0, last_interaction_at: 1.hour.ago, abandoned: false) }
+  let!(:old_cart)    { Cart.create!(total_price: 50.0, last_interaction_at: 8.days.ago, abandoned: true) }
+  let!(:stale_cart)  { Cart.create!(total_price: 75.0, last_interaction_at: 4.hours.ago, abandoned: false) }
+
+  it "marks carts inactive for more than 3 hours as abandoned" do
+    expect {
+      AbandonedCartsJob.perform_now
+    }.to change { stale_cart.reload.abandoned }.from(false).to(true)
+  end
+
+  it "does not mark recently active carts as abandoned" do
+    expect {
+      AbandonedCartsJob.perform_now
+    }.not_to change { active_cart.reload.abandoned }
+  end
+
+  it "destroys carts abandoned for more than 7 days" do
+    expect {
+      AbandonedCartsJob.perform_now
+    }.to change { Cart.exists?(old_cart.id) }.from(true).to(false)
+  end
 end
